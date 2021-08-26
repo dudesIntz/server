aff645d50bb0a11f21592fb9e0cae172bf26538b
dudesIntz <75521749+dudesIntz@users.noreply.github.com>
2021-08-26 09:27:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const html = __dirname + '/dist/excologyui/';
+
+const port = 4000;
+
+// Express
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const express = require('express');
+var app = express();
+
+app
+    .use(compression())
+    .use(bodyParser.json())
+    // Static content
+    .use(express.static(html))
+    // Default route
+    .use(function(req, res) {
+      res.sendFile(html + 'index.html');
+    })
+    // Start server
+    .listen(port, function () {
+        console.log('Port: ' + port);
+        console.log('Html: ' + html);
+    });
